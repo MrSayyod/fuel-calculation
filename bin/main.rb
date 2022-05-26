ded35072506cad8994b37b7ea5c6129bde1f0fbc
MrSayyod <ssayyod@gmail.com>
2022-05-26 08:54:54 -0700

    Create Flight class

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,11 @@
 require_relative '../lib/spaceship.rb'
+
+class Flight
+  def initialize(spaceship_mass, flight_path)
+    @flight_path = flight_path
+    @spaceship = nil
+    @total_fuel = 0
+    create_spaceship(spaceship_mass)
+    flight_journey
+  end
+end
