39dc3fd1c037159f851ebff5f41e1c82e67ea809
MrSayyod <ssayyod@gmail.com>
2022-05-26 08:56:53 -0700

    Make examples

--- bin/main.rb
+++ bin/main.rb
@@ -20,3 +20,7 @@ class Flight
     puts "Total fuel for the trip is #{@total_fuel}"
   end
 end
+
+Flight.new(28801, [[:launch, 9.807], [:land, 1.62], [:launch, 1.62], [:land, 9.807]])
+Flight.new(14606, [[:launch, 9.807], [:land, 3.711], [:launch, 3.711], [:land, 9.807]])
+Flight.new(75432, [[:launch, 9.807], [:land, 1.62], [:launch, 1.62], [:land, 3.711], [:launch, 3.711], [:land, 9.807]])
