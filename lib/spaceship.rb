6eeb4557ec1ebf6f1f5afd3f1d5422fda9c223f2
MrSayyod <ssayyod@gmail.com>
2022-05-26 08:49:53 -0700

    Add calculation function for launching

--- lib/spaceship.rb
+++ lib/spaceship.rb
@@ -3,4 +3,15 @@ class Spaceship
   def initialize(mass)
     @mass = mass
   end
+
+  def launch(mass, gravity)
+    fuel = (mass * gravity * 0.042 - 33).floor
+    extra_fuel = fuel
+    while extra_fuel > 0
+      extra_fuel = (extra_fuel * gravity * 0.042 - 33).floor
+      fuel += extra_fuel if extra_fuel.positive?
+    end
+    fuel
+  end
+
 end
