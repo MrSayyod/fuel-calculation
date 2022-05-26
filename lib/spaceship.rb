0659749225908d1404431e6b18cd907b9b11a0b6
MrSayyod <ssayyod@gmail.com>
2022-05-26 09:12:51 -0700

    Fix linter errors

--- lib/spaceship.rb
+++ lib/spaceship.rb
@@ -1,5 +1,6 @@
 class Spaceship
   attr_reader :mass
+
   def initialize(mass)
     @mass = mass
   end
@@ -7,7 +8,7 @@ class Spaceship
   def launch(mass, gravity)
     fuel = (mass * gravity * 0.042 - 33).floor
     extra_fuel = fuel
-    while extra_fuel > 0
+    while extra_fuel.positive?
       extra_fuel = (extra_fuel * gravity * 0.042 - 33).floor
       fuel += extra_fuel if extra_fuel.positive?
     end
@@ -17,9 +18,9 @@ class Spaceship
   def land(mass, gravity)
     fuel = (mass * gravity * 0.033 - 42).floor
     extra_fuel = fuel
-    while extra_fuel > 0
+    while extra_fuel.positive?
       extra_fuel = (extra_fuel * gravity * 0.033 - 42).floor
-      fuel+= extra_fuel if extra_fuel.positive?
+      fuel += extra_fuel if extra_fuel.positive?
     end
     fuel
   end
